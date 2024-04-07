c1f848b836f913bac8cbb57ba704de29bb77adb0
adegbite mohammed <adedamola13@gmail.com>
2024-04-07 14:23:43 +0100

    Adding this file to my java projects

---
+++ Kata.java
@@ -0,0 +1,104 @@
+public class Kata{
+	
+
+	public static boolean isEvenOrOdd(int number){
+		if (number % 2 == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+
+	}
+
+
+	public static int subtract(int number1, int number2){
+		int subtraction = number1 - number2;
+		if (subtraction < 0){
+			return subtraction * -1;
+		}
+
+		else {
+			return subtraction;
+		} 
+		
+	}
+
+	public static float divide(float numberOne, float numberTwo){
+	float division = numberOne / numberTwo;
+	float quotient = 0;
+	if(numberTwo == 0){
+
+		return quotient;
+
+		
+	}
+
+	else {
+		
+
+		return  division = numberOne / numberTwo;
+
+		}
+
+	 public static boolean isPrimeNumber(){
+	
+		int factor = 0;
+         for(int counter = 1; counter <= number/2; count++){
+
+         if(counter % number == 0) { 
+		
+		counter++; 
+		}
+          else if(counter == 1) {
+		
+		return true;
+	}
+	else {
+		s
+		return false;
+		}
+     	    }
+	}
+
+
+	public static integer factorOf(){
+
+	counter = 0;
+	
+	factor = 0;
+
+	for(int counter = 1; counter <= number; counter++) {
+
+	factor++;
+
+	if(counter % number ==0){
+
+		counter++;
+
+	}
+		return factor;
+
+	public static number factorialOf() {
+
+	int number = 0;
+
+	int factrial = 1;
+
+	while( factorial <= number){
+
+	if(number % factor == 0 )
+
+	facrorial++;
+
+	}
+
+	return factorial 
+
+
+	}
+
+	
+
+	}
+}
