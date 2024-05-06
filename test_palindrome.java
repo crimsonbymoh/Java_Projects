8aca2e4c0fb5eacd448f92a32d012aa5ed1e9d21
adegbite mohammed <adedamola13@gmail.com>
2024-05-06 10:23:35 +0100

    Adding this to my existing folder

---
+++ test_palindrome.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PalindromeTest{
+
+	@Test
+	public void testIfNumberIsAPalindrome(){
+		boolean result = palindromeChecker
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
